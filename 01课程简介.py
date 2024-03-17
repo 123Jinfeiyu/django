be7122cb61392fb05c0a229e4e67105196cba2ac
123Jinfeiyu <125663250+123Jinfeiyu@users.noreply.github.com>
2024-03-17 23:18:50 +0800

    Add files via upload

---
+++ 01课程简介.py
@@ -0,0 +1,32 @@
+"""
+课程安排：
+
+第一阶段：Linux
+
+第二阶段：MySQL数据库和redis
+
+
+第三阶段：Django框架
+
+
+第四阶段：Django项目
+
+总时长：3个多月
+
+课程名称：Django后端开发
+
+
+网站  小程序  app(抖音)
+
+
+
+操作系统：
+windows  macOS  Linux
+
+windows：视窗操作系统
+
+macos:苹果系统
+
+Linux：服务器操作系统
+
+"""
